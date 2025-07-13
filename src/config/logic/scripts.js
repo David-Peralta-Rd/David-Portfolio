f3399091e8e90f707c0b6eb99b0a37790e26cd4b
David-Peralta-Rd <crdavidperalta@gmail.com>
2025-07-13 16:45:37 -0500

    update mobile view

--- src/config/logic/scripts.js
+++ src/config/logic/scripts.js
@@ -3,7 +3,8 @@ let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x')
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
 }
 
 
@@ -29,4 +30,7 @@ window.onscroll = () => {
     // Stick header
     let header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 }
